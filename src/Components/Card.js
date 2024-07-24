1412e3592d7e898523d70fd8d2e64d6996861e8d
SharjeelCh <sharjeelh6451@gmail.com>
2024-07-24 20:36:07 +0500

    transform

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,7 +8,7 @@ function Card({ text, image }) {
       <button onClick={()=>{navigate("/QuizPage",{state:text})}}>
         <img
           src={image}
-          className="w-32 h-32 hover:opacity-80 transition-all rounded-t-md object-cover"
+          className="w-32 h-32 hover:opacity-95 transition-all rounded-t-md object-cover hover:transition-transform hover:scale-105"
         />
       </button>
       <button className="flex justify-center  items-center py-1 text-blue-800 hover:text-blue-600 font-bold bg-lime-200 hover:bg-lime-300 transition-all rounded-b-md">
