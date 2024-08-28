3f5543561b5697612b76ead74a2427e970f45585
sharjeelch <sharjeelh6451@gmail.com>
2024-08-28 20:53:23 +0500

    ads

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
- origin: ["http://localhost:3000"],
+ origin: ["http://localhost:3000","https://vercel.com/sharjeel-fida-chs-projects/quizmo-1sty"],
  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
  allowedHeaders: ["Content-Type", "Authorization"],
  credentials: true,
